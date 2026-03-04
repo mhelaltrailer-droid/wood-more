7cfc33dca57147f6dd13da49d2c7e7accf352670
John Bedeir <johnbedeir@Johns-MacBook-Pro.local>
2026-03-04 07:11:05 -0500

    init storage before runApp for API mode

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,11 @@ import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'core/app_theme.dart';
 import 'screens/login_screen.dart';
+import 'services/storage_service.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await initStorage();
   runApp(const WoodAndMoreApp());
 }
 
